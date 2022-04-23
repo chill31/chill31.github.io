964f91aaeaa137ae48b89309b55ee2e9aa6528ba
chill31 <89910163+chill31@users.noreply.github.com>
2022-04-23 12:12:03 +0530

    Deleted SERVER code

--- index.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-fs.writeFileSync("./server/things.json", "{ \"name\": [1, 5, 8] }");
-
-const dara = fs.readFileSync("./server/things.json", { encoding: 'utf8', flag: 'r'});
-
-console.log(dara);
