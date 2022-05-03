e638709aa0b5837fb4d380a847c5252dab30ec59
chill31 <89910163+chill31@users.noreply.github.com>
2022-05-03 13:37:47 +0530

    Updated utility
    updated utility for export intents

--- js/util/export/utility.js
+++ js/util/export/utility.js
@@ -7,7 +7,7 @@ class Utility {
     /**
      * 
      * @param {URL} location 
-     * @fires newPage
+     * @fires opens new page
      */
 
     openNewPage(location){
@@ -16,7 +16,8 @@ class Utility {
 
     /**
      * 
-     * @returns {documentSelectedText}
+     * @returns selected text
+     * @fires gets selected text by the user
      */
 
     getSelectionText(){
@@ -26,11 +27,86 @@ class Utility {
     /**
      * 
      * @param {URL} location
-     * @fires changeLocation
+     * @fires changes window location to given location/path
      */
 
     redirect(location){
      window.location.href = location;
+    }
+
+    /**
+     * 
+     * @param {*} text
+     * @fires writes text to clipboard
+     */
+
+    copyText(text) {
+        navigator.clipboard.writeText(text);
+    }
+
+    /**
+     * @fires refreshes page
+     */
+
+    refreshPage(){
+        this.redirect(window.location.href);
+    }
+
+    /**
+     * 
+     * @param {number} n 
+     * @returns Formatted Date
+     */
+
+    formatDate(n){
+  
+        switch(n){
+      
+            case 1:
+                return "1st";
+            case 2:
+                return "2nd";
+            case 3:
+                return "3rd";
+            case 21:
+                return "21st";
+            case 22:
+                return "22nd";
+            case 23:
+                return "23rd";
+            case 31:
+                return "31st";
+            default:
+                return `${n}th`;
+      
+        }
+      
+    }
+
+    /**
+     * 
+     * @param {number} d 
+     * @returns day according to number
+     */
+
+    fixDay(d){
+
+        const fixDayDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        return fixDayDays[d];
+      
+    }
+
+    /**
+     * @param {number} m
+     * @returns month according to number
+     */
+
+     fixMonth(m){
+
+        const fixMonthsMonths = [0, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      
+        return fixMonthsMonths[m];
+      
     }
 
 }
