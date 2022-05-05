4cbe4a89b4be3c7ade5d957c803dbbed698f32f8
chill31 <89910163+chill31@users.noreply.github.com>
2022-05-05 18:58:41 +0530

    Added favourites
    added favourite options to projects when activating grid view display

--- js/projects.js
+++ js/projects.js
@@ -15,6 +15,8 @@ fetch("/assets/web/projects.json").then((res) => res.json()).then((data) => {
 
     gridOptionBtn.addEventListener("click", () => {
 
+      grid_once = true;
+
       sliderOptionBtn.classList.remove("selected");
       gridOptionBtn.classList.add("selected");
 
@@ -180,6 +182,7 @@ fetch("/assets/web/projects.json").then((res) => res.json()).then((data) => {
       created.classList.add("grid-card");
 
       created.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="star star${i}"><path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/></svg>
 <h2>${data.projects[i].title}</h2>
 
 <p>${data.projects[i].description}</p>
@@ -190,4 +193,43 @@ fetch("/assets/web/projects.json").then((res) => res.json()).then((data) => {
       gridContainer.append(created);
 
     }
+
+    let fav_projects = [];
+    const stars = document.querySelectorAll(".star");
+
+    if(!localStorage.getItem("favourite_projects")) {
+      fav_projects = [];
+    }
+
+    if(localStorage.getItem("favourite_projects")) {
+      const localItem = JSON.parse(localStorage.getItem("favourite_projects"));
+      fav_projects = localItem;
+
+      for(let i = 0; i < data.projects.length; i++){
+        if(localItem[i] == true){
+          stars[i].classList.add("active");
+        } else if(localItem[i] == false){
+          stars[i].classList.remove("active");
+        }
+      }
+
+    }
+
+    for(let i = 0; i < data.projects.length; i++) {
+      fav_projects.push(false);
+
+      stars[i].addEventListener("click", () => {
+        stars[i].classList.toggle("active");
+
+        if(stars[i].classList.contains("active")) {
+          fav_projects[i] = true;
+        } else if(!stars[i].classList.contains("active")) {
+          fav_projects[i] = false;
+        }
+
+        localStorage.setItem("favourite_projects", JSON.stringify(fav_projects));
+      });
+
+    }
+
 });
