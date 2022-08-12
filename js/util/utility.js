33e1930977ebf9e7a2b429e3f3d22bf6c67c2fd1
chill31 <89910163+chill31@users.noreply.github.com>
2022-08-12 12:51:43 +0530

    Removed 'Updates' from shortcuts and overlay menu

--- js/util/utility.js
+++ js/util/utility.js
@@ -268,9 +268,7 @@ createdHamMenu.innerHTML = `
 
   <h1 class="menu-item item-projects item2" data-redirect="/projects/">Projects</h1>
 
-  <h1 class="menu-item item-updates item3" data-redirect="/Updates.html">Updates</h1>
-
-  <h1 class="menu-item item-settings item4" data-redirect="/Settings.html">Settings</h1>
+  <h1 class="menu-item item-settings item3" data-redirect="/Settings.html">Settings</h1>
 </div>
 
 `;
@@ -349,12 +347,6 @@ shortCutContainer.innerHTML = `
   <a href="/Projects.html" data-redirect="Projects" class="shortcut short-projects">
 
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 144v288c0 26.5-21.5 48-48 48h-416C21.5 480 0 458.5 0 432v-352C0 53.5 21.5 32 48 32h160l64 64h192C490.5 96 512 117.5 512 144z"/></svg>
-
-  </a>
-
-  <a href="/Updates.html" data-redirect="Updates" class="shortcut short-updates">
-
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M160 32V64H288V32C288 14.33 302.3 0 320 0C337.7 0 352 14.33 352 32V64H400C426.5 64 448 85.49 448 112V160H0V112C0 85.49 21.49 64 48 64H96V32C96 14.33 110.3 0 128 0C145.7 0 160 14.33 160 32zM0 192H448V464C448 490.5 426.5 512 400 512H48C21.49 512 0 490.5 0 464V192zM328.1 304.1C338.3 295.6 338.3 280.4 328.1 271C319.6 261.7 304.4 261.7 295 271L200 366.1L152.1 319C143.6 309.7 128.4 309.7 119 319C109.7 328.4 109.7 343.6 119 352.1L183 416.1C192.4 426.3 207.6 426.3 216.1 416.1L328.1 304.1z"/></svg>
 
   </a>
 
